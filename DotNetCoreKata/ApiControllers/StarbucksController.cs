8d343ec8127118d4d1c958ca41fab04bf16b9720
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2024-01-28 15:27:35 +0800

    feat (Swagger): use NSwag

---
+++ DotNetCoreKata/ApiControllers/StarbucksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreKata.ApiControllers;
+
+public class StarbucksController: ControllerBase
+{
+    [HttpGet("/aaa")]
+    public IActionResult GetMenu()
+    {
+        return Ok();
+    }
+}
--- DotNetCoreKata/Program.cs
+++ DotNetCoreKata/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,6 +14,17 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();

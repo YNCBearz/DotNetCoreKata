6752b0fa5922921e90f020c30c55fad6fb810cd1
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2024-06-11 00:06:09 +0800

    Revert "feat(aoe): simple factory"
    This reverts commit 2f188a581bc725aaf4a4c023709c3ae879ec3330.

--- DotNetCoreKata/Services/AgeOfEmpires/Program.cs
+++ DotNetCoreKata/Services/AgeOfEmpires/Program.cs
@@ -7,6 +7,16 @@ public class Program
 {
     public IUnit Train(UnitCategory unitCategory)
     {
-        return TrainingCenter.Create(unitCategory);
+        var militia = new Unit(new Stick(), new Legs());
+        var archer = new Unit(new Bow(), new Legs());
+        var knight = new Unit(new Sword(), new Horse());
+
+        return unitCategory switch
+        {
+            UnitCategory.Military => militia,
+            UnitCategory.Archer => archer,
+            UnitCategory.Knight => knight,
+            _ => militia
+        };
     }
 }

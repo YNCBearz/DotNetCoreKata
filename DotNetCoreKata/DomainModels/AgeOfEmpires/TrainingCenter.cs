2f188a581bc725aaf4a4c023709c3ae879ec3330
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2024-06-11 00:06:02 +0800

    feat(aoe): simple factory

---
+++ DotNetCoreKata/DomainModels/AgeOfEmpires/TrainingCenter.cs
@@ -0,0 +1,17 @@
+﻿using DotNetCoreKata.Enums;
+
+namespace DotNetCoreKata.DomainModels.AgeOfEmpires;
+
+public static class TrainingCenter
+{
+    public static IUnit Create(UnitCategory unitCategory)
+    {
+        return unitCategory switch
+        {
+            UnitCategory.Military => new Unit(new Stick(), new Legs()),
+            UnitCategory.Archer => new Unit(new Bow(), new Legs()),
+            UnitCategory.Knight => new Unit(new Sword(), new Horse()),
+            _ => new Unit(new Stick(), new Legs())
+        };
+    }
+}

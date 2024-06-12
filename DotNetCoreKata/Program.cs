be94152aa4a2cddd7928e25ae82b8b1176083ff8
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2024-06-12 13:58:39 +0800

    chore: change swagger plugin

--- DotNetCoreKata/Program.cs
+++ DotNetCoreKata/Program.cs
@@ -23,12 +23,18 @@ builder.Host.UseSerilog();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApiDocument();
+
+// Swagger Gen
+builder.Services.AddSwaggerGen();
 
 // Dependency Injection
 builder.Services.AddTransient<IClient, Client>();
 
 var app = builder.Build();
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -36,17 +42,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-
-if (app.Environment.IsDevelopment())
-{
-    // Add OpenAPI 3.0 document serving middleware
-    // Available at: http://localhost:<port>/swagger/v1/swagger.json
-    app.UseOpenApi();
-
-    // Add web UIs to interact with the document
-    // Available at: http://localhost:<port>/swagger
-    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();

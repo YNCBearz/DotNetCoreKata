e0615a5bbedd93b29abbcbc5859f13bf4fd7ae49
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2024-08-28 07:24:24 +0800

    feat(MysteriousName): add test

--- DotNetCoreKata/Tests/UnitTests/Services/Overcooked/MysteriousName/BeverageStoreTests.cs
+++ DotNetCoreKata/Tests/UnitTests/Services/Overcooked/MysteriousName/BeverageStoreTests.cs
@@ -1,9 +1,36 @@
-﻿using NUnit.Framework;
+﻿using DotNetCoreKata.Services.Overcooked.MysteriousName;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace DotNetCoreKata.Tests.UnitTests.Services.Overcooked.MysteriousName;
 
 [TestFixture]
 public class BeverageStoreTests
 {
+    [Test]
+    public void bumble_tea()
+    {
+        // Arrange
+        var beverageStore = new BeverageStore();
+        
+        // Act
+        var drink = beverageStore.Get("qqneineidrinkabletomeiputea", "half sugar", "less ice");
+        
+        // Assert
+        drink.GetContent().Should().Be("BubbleTea: half sugar, less ice");
+    }
     
+    [Test]
+    public void default_coke()
+    {
+        // Arrange
+        var beverageStore = new BeverageStore();
+        
+        // Act
+        var drink = beverageStore.Get("unknown", "no sugar", "less ice");
+        
+        // Assert
+        drink.GetContent().Should().Be("Coke: no sugar, less ice");
+    }
+
 }

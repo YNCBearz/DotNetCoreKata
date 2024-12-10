d6d9947a2baa9692a25040c7f86a37d3f99085f0
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2024-12-10 18:24:38 +0800

    feat(ska): update

--- UnitTestAndRefactorKata/Solutions/EmployeeSalaryCalculatorTests.cs
+++ UnitTestAndRefactorKata/Solutions/EmployeeSalaryCalculatorTests.cs
@@ -5,12 +5,52 @@ namespace UnitTestAndRefactorKata.Solutions;
 [TestFixture]
 public class EmployeeSalaryCalculatorTests
 {
+    private EmployeeSalaryCalculator _calculator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _calculator = new EmployeeSalaryCalculator();
+    }
+
     [Test]
     public void full_time_no_over_time()
     {
-        var calculator = new EmployeeSalaryCalculator();
-        var netSalary = calculator.GetNetSalary("Full-time",0.1m,1.5m, 100, 0);
-        
-        Assert.AreEqual(2700m, netSalary);
+        var netSalary = _calculator.GetNetSalary("Full-time", 0.1m, 1.5m, 100, 0);
+
+        Assert.That(netSalary, Is.EqualTo(2700m));
+    }
+
+    [Test]
+    public void part_time_no_over_time()
+    {
+        var netSalary = _calculator.GetNetSalary("Part-time", 0.1m, 1.5m, 100, 0);
+
+        Assert.That(netSalary, Is.EqualTo(1800m));
+    }
+
+    [Test]
+    public void intern_no_over_time()
+    {
+        var netSalary = _calculator.GetNetSalary("Intern", 0.1m, 1.5m, 100, 0);
+
+        Assert.That(netSalary, Is.EqualTo(1350m));
+    }
+
+    [Test]
+    public void contractor_no_over_time()
+    {
+        var netSalary = _calculator.GetNetSalary("Contractor", 0.1m, 1.5m, 100, 0);
+
+        Assert.That(netSalary, Is.EqualTo(4500m));
+    }
+
+    [Test]
+    public void unknown_type_throw_exception()
+    {
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            _calculator.GetNetSalary("Unknown", 0.1m, 1.5m, 100, 0);
+        });
     }
 }

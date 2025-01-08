21753f870a4298f6c58e903a5c9d37d503e0db5a
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2025-01-08 16:11:13 +0800

    feat(interview): use init solution

--- DotNetCoreKata/Services/Interview/Question.cs
+++ DotNetCoreKata/Services/Interview/Question.cs
@@ -4,11 +4,14 @@ public class Question
 {
     public bool IsPowerOfTwo(int input)
     {
-        if (input <= 0)
-        {
-            return false;
-        }
-        
-        return (input & (input - 1)) == 0;
+        // if (input <= 0)
+        // {
+        //     return false;
+        // }
+        //
+        // return (input & (input - 1)) == 0;
+
+        var numberToTwo = Convert.ToString(input, 2);
+        return numberToTwo.Count(x => x == '1') == 1;
     }
 }

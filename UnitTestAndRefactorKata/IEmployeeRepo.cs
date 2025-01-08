45de637e3c489f9a9b7f81ff94b88810a0c76539
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2025-01-08 20:24:17 +0800

    refactor: extract method and dictionary

---
+++ UnitTestAndRefactorKata/IEmployeeRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace UnitTestAndRefactorKata;
+
+public interface IEmployeeRepo
+{
+    int GetHoursWorked(string employeeId, int month, int year);
+    decimal GetEmployeeBonus(string employeeId, string employeeType);
+}

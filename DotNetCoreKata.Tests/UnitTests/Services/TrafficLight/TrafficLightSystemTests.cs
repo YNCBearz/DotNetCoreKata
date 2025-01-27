cca930f3c354cd3ff90fbd2f8b4f2f63f4404d2d
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2025-01-27 23:23:59 +0800

    refactor: Remove TrafficLightSystem unit tests

---
+++ DotNetCoreKata.Tests/UnitTests/Services/TrafficLight/TrafficLightSystemTests.cs
@@ -0,0 +1,51 @@
+using DotNetCoreKata.DomainModels.TrafficLight;
+using DotNetCoreKata.Services.TrafficLight;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetCoreKata.Tests.UnitTests.Services.TrafficLight;
+
+[TestFixture]
+public class TrafficLightSystemTests : IDisposable
+{
+	private StringWriter? _consoleOutput;
+
+	[SetUp]
+	public void SetUp()
+	{
+		_consoleOutput = new StringWriter();
+		Console.SetOut(_consoleOutput);
+	}
+
+	[Test]
+	public void Green()
+	{
+		var nextTrafficLight = TrafficLightSystem.NextColor(new DomainModels.TrafficLight.TrafficLight("Red"));
+		CarShould(nextTrafficLight, "Drive");
+	}
+
+	private void CarShould(ITrafficLight nextTrafficLight, string expected)
+	{
+		nextTrafficLight.Check(new Car());
+		_consoleOutput?.ToString().Should().Be(expected);
+	}
+
+	[Test]
+	public void Yellow()
+	{
+		var nextTrafficLight = TrafficLightSystem.NextColor(new DomainModels.TrafficLight.TrafficLight("Green"));
+		CarShould(nextTrafficLight, "Stop");
+	}
+
+	[Test]
+	public void Red()
+	{
+		var nextTrafficLight = TrafficLightSystem.NextColor(new DomainModels.TrafficLight.TrafficLight("Yellow"));
+		CarShould(nextTrafficLight, "Stop");
+	}
+
+	public void Dispose()
+	{
+		_consoleOutput?.Dispose();
+	}
+}
